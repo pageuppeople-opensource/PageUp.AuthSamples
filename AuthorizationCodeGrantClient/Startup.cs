4b7ef31bcad95d761f226e64b8ec8ba8c31628de
scottm <scottm@pageuppeople.com>
2017-06-19 17:09:51 +1000

    Working auth code grant flow

---
+++ AuthorizationCodeGrantClient/Startup.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+using System.IdentityModel.Tokens.Jwt;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+
+namespace MvcClient
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationScheme = "Cookies"
+            });
+
+
+            app.UseOpenIdConnectAuthentication(new OpenIdConnectOptions
+            {
+                AuthenticationScheme = "PageUp",
+                SignInScheme = "Cookies",
+
+                Authority = "https://testus.loginuat.pageuppeople.com",
+                RequireHttpsMetadata = false,
+
+                ClientId = "adsf", // TODO: enter credds here
+                ClientSecret = "df",
+
+                ResponseType = "code",
+                Scope = { "Compliance.Write", "Compliance.Read" },
+
+                GetClaimsFromUserInfoEndpoint = false,
+                SaveTokens = true,
+                AuthenticationMethod = OpenIdConnectRedirectBehavior.RedirectGet,
+                BackchannelHttpHandler = new TokenMessageHandler()
+
+            });
+            
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+
+    public class TokenMessageHandler : HttpClientHandler
+    {
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            Debug.WriteLine("Process request");
+            // Call the inner handler.
+            var response = await base.SendAsync(request, cancellationToken);
+            Debug.WriteLine($"Process response: {await response.Content.ReadAsStringAsync()}");
+            return response;
+        }
+    }
+}
